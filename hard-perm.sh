b45f18c7b0307b4bd7da7cc399da0a2562f93de6
stkisengese <skisengese@outlook.com>
2025-12-08 13:06:41 +0300

    fix: update permissions for hard-perm/0 and hard-perm/A to 401

--- hard-perm.sh
+++ hard-perm.sh
@@ -1,4 +1,4 @@
-chmod 701 hard-perm/0
+chmod 401 hard-perm/0
 chmod 402 hard-perm/1
 chmod 604 hard-perm/2
 chmod 777 hard-perm/3
@@ -8,4 +8,4 @@ chmod 460 hard-perm/6
 chmod 510 hard-perm/7
 chmod 604 hard-perm/8
 chmod 402 hard-perm/9
-chmod 701 hard-perm/A
+chmod 401 hard-perm/A
